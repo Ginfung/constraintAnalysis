9e952b1681f0fe9f7324fa73973c92105cb95fd8
Jianfeng <ginfungchan@gmail.com>
2015-02-10 13:27:35 -0500

    define DTLZ1

---
+++ DEMO/DTLZ1.m
@@ -0,0 +1,21 @@
+function [ f ] = DTLZ1(x,n)
+M = 4; %Here can set up the M
+k = 5; %Let k = 5, as suggested. Thus n should be 8
+%get g
+sum = 0;
+for i = M:M+k-1
+    sum = sum + ((x(i)-0.5)^2-cos(20*pi*(x(i)-0.5)));
+end
+g = 100 * (k + sum);
+%get f
+f = zeros(1,M);
+f(M) = 0.5*(1+g);
+for i = M-1:-1:1
+    f(i) = f(i+1)*x(M-i);
+end
+for i = 2:M
+    f(i) = f(i) * (1 - x(M+1-i));
+end
+
+end
+
