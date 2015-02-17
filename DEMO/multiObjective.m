433a733651c559d1483a6b5e3f22e0ee88003dca
Jianfeng <ginfungchan@gmail.com>
2015-02-16 21:23:52 -0500

    new boundary repair

--- DEMO/multiObjective.m
+++ DEMO/multiObjective.m
@@ -1,13 +1,13 @@
 %trial version for multi objective
 %modified based on DEB et al.: A fast and elitist multiobjective GA:NSGA-II
 clear all
-Problem = @DTLZ2;
+Problem = @DTLZ1;
 NP = 30; % MUAT LARGER THAN OBJECTIVEMIMENSION AND D!
 CR = 0.45;
 F = 0.3;
-gen_max = 50;
-D = 15;
-ObjectiveDimension = 6;
+gen_max = 200;
+D = 7;
+ObjectiveDimension = 3;
 domain_a = 0;
 domain_b = 1;
 
@@ -33,13 +33,15 @@ while (count <= gen_max)
         c = rev(3);
 
         mutant = parent(c,:) + F*(parent(a,:)-parent(b,:));
-        for x = 1:D %always check the variables domain
-            mutant(x) = max(domain_a,mutant(x));
-            mutant(x) = min(domain_b,mutant(x));
-        end
         for k = 1:D
             if (rand()<CR || k == D)
                 trial(k) = mutant(k);
+                if trial(k) < domain_a
+                    trial(k) = (parent(i,k)+domain_a)/2;
+                end
+                if trial(k) > domain_b
+                    trial(k) = domain_a+(domain_b-parent(i,k))/2;
+                end
             else
                 trial(k) = parent(i,k);
             end
@@ -80,7 +82,7 @@ while (count <= gen_max)
             i = i+1;
         end 
     end
-   count = count + 1
+   count = count + 1;
 end
 
 for i = 1:NP
