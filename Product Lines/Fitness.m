8bee8316fe3bfb25664041e3e709a7f1ad84c69a
Jianfeng <ginfungchan@gmail.com>
2015-03-16 00:00:32 -0400

    more details add in IBEA

--- Product Lines/Fitness.m
+++ Product Lines/Fitness.m
@@ -1,7 +1,18 @@
-function [ output_args ] = Fitness( input_args )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+function [ Fitness ] = Fitness( P,obj )
+global k;
+global ObjectiveDimension;
 
+sizeofP = size(P,1);
+Fitness = zeros(1,sizeofP);
 
+for  i = 1:sizeofP
+    for j = 1:sizeofP
+        if j == i
+            continue;
+        end
+        indicator = epsAddIndicator(obj(j,:),obj(i,:),ObjectiveDimension);
+        Fitness(i) = Fitness(i) -exp(-indicator/k) ;
+    end
+end
 end
 
--- Product Lines/epsAddIndicator.m
+++ Product Lines/epsAddIndicator.m
@@ -1,7 +1,7 @@
 % return the epsAddIndicator
 % reference: Zitzler, Eckart, and Simon Künzli. "Indicator-based selection in multiobjective search." Parallel Problem Solving from Nature-PPSN VIII. Springer Berlin Heidelberg, 2004.
 function [ indicator ] = epsAddIndicator( f_A,f_B,objectiveDimension )
-indicator = f_A(0) - f_B(0);
+indicator = f_A(1) - f_B(1);
 for i = 2:objectiveDimension
     temp = f_A(i) - f_B(i);
     if(temp > indicator)
