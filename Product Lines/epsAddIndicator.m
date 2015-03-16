5b8a614721d713ab5c9d21265e1c4262c4dee3f3
Jianfeng <ginfungchan@gmail.com>
2015-03-16 11:11:52 -0400

    indicator bug fix

--- Product Lines/epsAddIndicator.m
+++ Product Lines/epsAddIndicator.m
@@ -1,9 +1,12 @@
 % return the epsAddIndicator
 % reference: Zitzler, Eckart, and Simon Künzli. "Indicator-based selection in multiobjective search." Parallel Problem Solving from Nature-PPSN VIII. Springer Berlin Heidelberg, 2004.
 function [ indicator ] = epsAddIndicator( f_A,f_B,objectiveDimension )
-indicator = f_A(1) - f_B(1);
+global objBound_Min;
+global objBound_Max;
+
+indicator = (f_A(1) - f_B(1))/(objBound_Max(1) - objBound_Min(1));
 for i = 2:objectiveDimension
-    temp = f_A(i) - f_B(i);
+    temp = (f_A(i) - f_B(i))/(objBound_Max(i) - objBound_Min(i));
     if(temp > indicator)
         indicator = temp;
     end
